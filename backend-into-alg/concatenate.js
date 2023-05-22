334617bc2b5e0de2fab17c967cf67209260deeed
Glaucielle Sá <glauciellesa@gmail.com>
2023-05-22 10:10:02 +0200

    concatenate exercise

---
+++ backend-into-alg/concatenate.js
@@ -0,0 +1,7 @@
+const frase1 = "Hello";
+/* II - concatenate sentence
+Write an algorithm which concatenates two phrases together. */
+
+const frase2 = " Glaucielle";
+
+console.log(frase1 + frase2);
