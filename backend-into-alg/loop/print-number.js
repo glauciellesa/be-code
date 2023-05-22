ec19665bac943eed1ac526ccc06faa80b24a8e79
Glaucielle Sá <glauciellesa@gmail.com>
2023-05-22 16:29:25 +0200

    print-number loop exercise

---
+++ backend-into-alg/loop/print-number.js
@@ -0,0 +1,44 @@
+/* I - print numbers
+Write an algorithms which receives an integers n and prints:
+
+the numbers from 1 to n
+
+the numbers from 1 to n in descending order
+
+the numbers from -n to n
+
+the odd numbers from 1 to n */
+
+const printNumber = (number) => {
+  for (let i = 1; i <= number; i++) {
+    console.log(i);
+  }
+};
+
+/* printNumber(9); */
+
+const printNumberDescending = (number) => {
+  for (let i = number; i >= 1; i--) {
+    console.log(i);
+  }
+};
+
+/* printNumberDescending(9); */
+
+const printFromNegativeTopositive = (number) => {
+  for (let i = -number; i <= number; i++) {
+    console.log(i);
+  }
+};
+
+printFromNegativeTopositive(3);
+
+const printOddNumber = (number) => {
+  for (let i = 1; i <= number; i++) {
+    if (i % 2 != 0) {
+      console.log(i);
+    }
+  }
+};
+
+/* printOddNumber(9); */
