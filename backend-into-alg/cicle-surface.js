af70e6e4d2761a37e7fac326334a9803f1b4977b
Glaucielle Sá <glauciellesa@gmail.com>
2023-05-22 10:48:57 +0200

    cicle-surface

---
+++ backend-into-alg/cicle-surface.js
@@ -0,0 +1,12 @@
+/*  Write an algorithm which receives the radius of a circle and calculate its surface.*/
+const pi = 3.14;
+let radius = 7.9;
+/* let valeu = pi * radius;
+console.log(valeu * 2); */
+
+function circumference(pi, radius) {
+  let valeu = pi * radius;
+  console.log(valeu * 2);
+}
+
+circumference(pi, radius);
