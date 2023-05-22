e4ba6687dedc3e6d5ac884aa5f90713eec22c747
Glaucielle Sá <glauciellesa@gmail.com>
2023-05-22 14:29:36 +0200

    Day exercise

--- backend-into-alg/conditions/days-number.js
+++ backend-into-alg/conditions/days-number.js
@@ -1,2 +1,33 @@
 /* IV - day’s number
 Write an algorithm which given the number of a day returns its name. */
+
+const printDay = () => {
+  const date = new Date();
+  const day = date.getDay();
+  switch (day) {
+    case 0:
+      return "Sunday";
+
+    case 1:
+      return "Monday";
+
+    case 3:
+      return "Tuesday";
+
+    case 4:
+      return "Wednesday";
+
+    case 5:
+      return "Thursday";
+
+    case 6:
+      return "Friday";
+
+    case 7:
+      return "Saturday";
+
+    default:
+      return "No a valid day";
+  }
+};
+console.log(printDay());
